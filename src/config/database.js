ae12c5f62a076b0150cc1bf79140443d9310ff2b
Sainky <sainkygujjar12@gmail.com>
2026-05-18 00:05:47 +0530

    Initial commit

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+
+
+async function connectToDB() {
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+
+        console.log("Connected to Database")
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
+
+module.exports = connectToDB
